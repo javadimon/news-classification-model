65b7499385eee959e55a4416178a0ab67759282d
zubanov.d <zubanov@gmail.com>
2022-05-22 13:48:19 +0300

    Added Selenium

--- src/train_data_loader.py
+++ src/train_data_loader.py
@@ -1,5 +1,8 @@
+from random import randrange
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
+
 
 def load():
     """
@@ -7,28 +10,36 @@ def load():
     """
 
     driver = webdriver.Chrome()
-
-    driver.get("https://habr.com/ru/hub/programming/")
-
     driver.implicitly_wait(0.5)
 
-    # tm-article-snippet__title-link
-    # tm - article - snippet__hubs
+    urls = ["https://habr.com/ru/hub/programming/", "https://habr.com/ru/hub/nix/", "https://habr.com/ru/hub"
+                                                                                    "/machine_learning/"]
+    for url in urls:
+        driver.get(url)
 
-    contents = driver.find_elements(By.CLASS_NAME, "article-formatted-body")
-    print(len(contents))
-    for content in contents:
-        print(content.text)
-        print("\n\n-----------------------------------------------------\n\n")
+        # tm-article-snippet__title-link
+        tittles = driver.find_elements(By.CLASS_NAME, "tm-article-snippet__title-link")
+        # tm - article - snippet__hubs
+        contents = driver.find_elements(By.CLASS_NAME, "article-formatted-body")
 
-    driver.implicitly_wait(2.5)
+        print(len(tittles))
+        print(len(contents))
+
+        index = 0
+        for content in contents:
+            print(tittles[index].text + "\n")
+            print(content.text)
+            print("\n\n-----------------------------------------------------\n\n")
+
+        timeout = randrange(2, 6, 2)
+        print("Timeout: " + str(timeout))
+        time.sleep(timeout)
 
     driver.quit()
 
 
 if __name__ == '__main__':
     load()
-
 
 # Биржа
 # https://bcs-express.ru/category/rossiyskiy-rynok
