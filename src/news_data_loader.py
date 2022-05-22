d95d4f38f7c6ec62cd3ccf84efca543bf3110f53
zubanov.d <zubanov@gmail.com>
2022-05-22 05:54:10 +0300

    Init repo

--- src/news_data_loader.py
+++
@@ -1,8 +0,0 @@
-def load():
-    """
-    Loads the news data from the URLs.
-    """
-
-
-if __name__ == '__main__':
-    load()
---
+++ src/train_data_loader.py
@@ -0,0 +1,24 @@
+def load():
+    """
+    Loads the news data from the URLs.
+    """
+
+
+if __name__ == '__main__':
+    load()
+
+
+# Биржа
+# https://bcs-express.ru/category/rossiyskiy-rynok
+
+# Железо
+# https://www.ixbt.com/news/
+
+# Программирование
+# https://habr.com/ru/hub/programming/
+
+# Linux
+# https://habr.com/ru/hub/nix/
+
+# AI / ML
+# https://habr.com/ru/hub/machine_learning/
