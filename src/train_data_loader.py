f7cd63c01ff1369a5c5c3f3fa29a1e99a46c52fa
zubanov.d <zubanov@gmail.com>
2022-05-28 12:51:26 +0300

    Added Selenium

--- src/train_data_loader.py
+++ src/train_data_loader.py
@@ -2,6 +2,7 @@ from random import randrange
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import codecs
 
 
 def load():
@@ -13,8 +14,7 @@ def load():
     driver.implicitly_wait(0.5)
 
     class_index = 1
-    urls = ["https://habr.com/ru/hub/programming/", "https://habr.com/ru/hub/nix/", "https://habr.com/ru/hub"
-                                                                                    "/machine_learning/"]
+    urls = ["https://habr.com/ru/hub/programming/", "https://habr.com/ru/hub/nix/", "https://habr.com/ru/hub/machine_learning/"]
     pages = range(1, 51)
     for base_url in urls:
         for page in pages:
@@ -33,10 +33,11 @@ def load():
             for content in contents:
                 csv_line = "\"" + str(class_index) + "\",\"" + normalize_string(tittles[index].text) + "\",\"" + normalize_string(content.text) + "\""
                 write_line_to_file(csv_line)
+                print(url + " --- " + csv_line)
                 index += 1
 
             timeout = randrange(5, 10, 2)
-            print("Timeout: " + str(timeout))
+            print("------------ Timeout: " + str(timeout) + " ------------")
             time.sleep(timeout)
 
         class_index += 1
@@ -45,12 +46,13 @@ def load():
 
 
 def write_line_to_file(line):
-    with open("train-data-source/train.csv", "a") as file:
+    # with open("train-data-source/train.csv", "a") as file:
+    with codecs.open("train-data-source/train.csv", "a", "utf-8") as file:
         file.write(line + "\n")
 
 
 def normalize_string(s):
-    return s.replace("\"", "\"\"").replace("$", "\\$")
+    return s.replace("\"", "\"\"").replace("$", "\\$").replace("\n", " ").replace("\r", " ")
 
 
 if __name__ == '__main__':
