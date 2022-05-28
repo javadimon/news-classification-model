8e863080a7c794b731fee665679baad7a2e0ebaf
zubanov.d <zubanov@gmail.com>
2022-05-28 07:24:54 +0300

    Init repo

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
+import os
+
+
 def my_test():
+    if os.path.exists("train-data-source/test.csv"):
+        os.remove("train-data-source/test.csv")
+
     class_index = 1
     urls = ["https://habr.com/ru/hub/programming/", "https://habr.com/ru/hub/nix/", "https://habr.com/ru/hub"
                                                                                     "/machine_learning/"]
@@ -8,7 +14,6 @@ def my_test():
             url = base_url
             if page > 1:
                 url = base_url + "page" + str(page) + "/"
-            print("URL: " + url)
 
             # tm-article-snippet__title-link
             tittles = ["One", "Two", "Three $100"]
@@ -17,10 +22,16 @@ def my_test():
 
             index = 0
             for content in contents:
-                csv_line = str(class_index) + "," + normalize_string(tittles[index]) + "," + normalize_string(content)
-                print(csv_line)
+                csv_line = "\"" + str(class_index) + "\",\"" + normalize_string(tittles[index]) + "\",\"" + normalize_string(content) + "\""
+                # print(csv_line)
+                write_line_to_file(csv_line)
                 index += 1
         class_index += 1
+
+
+def write_line_to_file(line):
+    with open("train-data-source/test.csv", "a") as file:
+        file.write(line + "\n")
 
 
 def normalize_string(s):
